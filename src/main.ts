8f9b2e84072f3171629fba1ecb629a41387d5440
Helmer Bergström <hebe2400@student.miun.se>
2025-04-03 20:58:46 +0200

    Funktioner för att hämta kurser/URL, funktioner för att lagra i localstorage och hämta från localstorage.

--- src/main.ts
+++ src/main.ts
@@ -9,20 +9,21 @@ interface CourseInfo {
 async function fetchCourses(): Promise<CourseInfo[]> {
     const response = await fetch('https://webbutveckling.miun.se/files/ramschema_ht24.json');
     if(!response.ok) {
-        throw new Error('Fel!')
+        throw new Error('Fel!');
     }
 
     const courses: CourseInfo[] = await response.json();
     return courses;
-}
+};
 
 function saveCourses(courses: CourseInfo[]): void {
     localStorage.setItem('courses', JSON.stringify(courses));
-}
+};
 
+// Hämtar kurserna till "courses".
+// returnerar courses och omvandlar datan till array.
+// finns ingen data att hämta blir det en tom array.
 function getCourses(): CourseInfo[] {
     const courses = localStorage.getItem('courses');
     return courses ? JSON.parse(courses) : [] ;
-}
-
-fetchCourses().then(courses => console.log(courses));
+};
