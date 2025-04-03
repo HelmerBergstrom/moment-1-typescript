50e789c0b267270c5fb510ddbc4da9600f3e3503
Helmer Bergström <hebe2400@student.miun.se>
2025-04-03 20:56:57 +0200

    Skapar interface, funktion för att hämta URL/kurser, funktioner för att lagra i localstorage och hämta från localstorage.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,28 @@
-let fname: string = "Helmer";
+interface CourseInfo {
+    code: string;
+    name: string;
+    progression: 'A' | 'B' | 'C';
+    syllabus: string;
+}
 
-console.log(fname)
+// Promise att funktionen ska returnera en array "CourseInfo" när hämtningen är klar!
+async function fetchCourses(): Promise<CourseInfo[]> {
+    const response = await fetch('https://webbutveckling.miun.se/files/ramschema_ht24.json');
+    if(!response.ok) {
+        throw new Error('Fel!')
+    }
+
+    const courses: CourseInfo[] = await response.json();
+    return courses;
+}
+
+function saveCourses(courses: CourseInfo[]): void {
+    localStorage.setItem('courses', JSON.stringify(courses));
+}
+
+function getCourses(): CourseInfo[] {
+    const courses = localStorage.getItem('courses');
+    return courses ? JSON.parse(courses) : [] ;
+}
+
+fetchCourses().then(courses => console.log(courses));
